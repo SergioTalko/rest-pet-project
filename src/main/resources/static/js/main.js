20b498163ad265081abcfe7d831f87bdf301f90c
Sergio Talko <sergio.talko@gmail.com>
2018-07-03 12:36:26 +0300

    add Vue.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,47 @@
+var messageApi = Vue.resource('/message{/id}');
+
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div><i>({{message.id}})</i>{{message.text}}</div>'
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div>' +
+    '<message-row v-for="message in messages"  :message="message" />' +
+    '</div>',
+    created: function() {
+        messageApi.get().then(result =>
+        result.json().then(data =>
+        data.forEach(message => this.messages.push(message))
+    )
+    )
+    }
+});
+
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages" />',
     data: {
-        message: 'Привет, Vue!'
+        messages: []
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Привет, Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'Вы загрузили эту страницу в: ' + new Date().toLocaleString()
     }
 });
